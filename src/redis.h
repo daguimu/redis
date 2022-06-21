b07362ba388f60308c231249a4dee03a8ba5b2cc
daguimu <daguimu@keep.com>
2022-06-21 17:56:47 +0800

    feature: clean

--- src/redis.h
+++ src/redis.h
@@ -425,10 +425,15 @@ typedef long long mstime_t; /* millisecond time type. */
 #define REDIS_LRU_CLOCK_MAX ((1<<REDIS_LRU_BITS)-1) /* Max value of obj->lru */
 #define REDIS_LRU_CLOCK_RESOLUTION 1000 /* LRU clock resolution in ms */
 typedef struct redisObject {
+    // 类型
     unsigned type:4;
+    // 编码
     unsigned encoding:4;
+    // 对象最后一次被命令程序访问的时间；
     unsigned lru:REDIS_LRU_BITS; /* lru time (relative to server.lruclock) */
+    // 引用次数
     int refcount;
+    // 指向底层实现数据结构的指针
     void *ptr;
 } robj;
 
@@ -600,18 +605,27 @@ struct sharedObjectsStruct {
 
 /* ZSETs use a specialized version of Skiplists */
 typedef struct zskiplistNode {
+    // 成员对象
     robj *obj;
+    // 分值
     double score;
+    // 后退指针
     struct zskiplistNode *backward;
+    // 层
     struct zskiplistLevel {
+        // 前进指针
         struct zskiplistNode *forward;
+        // 跨度
         unsigned int span;
     } level[];
 } zskiplistNode;
 
 typedef struct zskiplist {
+    // 表头和表尾节点
     struct zskiplistNode *header, *tail;
+    // 表中节点的数量
     unsigned long length;
+    // 表中层数最大的节点的层数
     int level;
 } zskiplist;
 
